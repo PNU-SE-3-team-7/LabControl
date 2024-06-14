1ef781b05a18aac1ebe733b2b70bc68a5d369e89
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-06-14 23:29:13 +0300

    [LAB-1] Backend init

---
+++ src/main/java/com/pnu/lab/control/labcontrol/LabControlApplication.java
@@ -0,0 +1,13 @@
+package com.pnu.lab.control.labcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pnu/lab/control/labcontrol/LabControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnu.lab.control.labcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
