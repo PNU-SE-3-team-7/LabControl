18538d1e400d0301efd87eaa27c2aed76fc83252
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-06-17 19:22:00 +0300

    fix

--- src/main/java/com/pnu/lab/control/labcontrol/api/AssignmentController.java
+++ src/main/java/com/pnu/lab/control/labcontrol/api/AssignmentController.java
@@ -5,13 +5,22 @@ import com.pnu.lab.control.labcontrol.domain.Assignment;
 import com.pnu.lab.control.labcontrol.service.AssignmentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController("/assignment")
+@RestController
+@RequestMapping("/assignment")
 public class AssignmentController {
+
     private final AssignmentService service;
 
     @PostMapping
@@ -19,7 +28,7 @@ public class AssignmentController {
         return service.create(assignment);
     }
 
-    /*@PutMapping
+    @PutMapping
     public Assignment update(@Valid @RequestBody Assignment assignment) {
         return service.update(assignment);
     }
@@ -29,12 +38,14 @@ public class AssignmentController {
         return service.getList(searchRequest);
 
     }
+
     @GetMapping("/{id}")
     public Assignment getById(@PathVariable String id) {
         return service.findOne(id);
     }
+
     @DeleteMapping("/{id}")
     public void delete(@PathVariable String id) {
         service.delete(id);
-    }*/
+    }
 }
