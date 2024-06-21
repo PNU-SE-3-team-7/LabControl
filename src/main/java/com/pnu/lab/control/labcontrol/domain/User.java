afda4e9f590d59996402f797f07c109fea87e409
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-06-22 02:12:30 +0300

    [LAB-19] fix naming

--- src/main/java/com/pnu/lab/control/labcontrol/domain/User.java
+++ src/main/java/com/pnu/lab/control/labcontrol/domain/User.java
@@ -18,7 +18,7 @@ public class User extends BaseEntity {
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
-    private String last_name;
+    private String lastName;
     @Email
     @Column(name = "email")
     private String email;
