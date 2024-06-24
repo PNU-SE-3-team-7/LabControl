86cca3565635326f0b9935c07d4195e8c2d829ed
JJerome-NM <jjerome010@gmail.com>
2024-06-24 17:07:31 +0300

    [HOTFIX] Attempted to fix

--- src/main/java/com/pnu/lab/control/labcontrol/domain/CourseMember.java
+++ src/main/java/com/pnu/lab/control/labcontrol/domain/CourseMember.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,7 +28,7 @@ public class CourseMember {
     @Id
     @Column(name = "course_id")
     private String courseId;
-    @NotBlank
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(name = "member_type")
     private MemberType memberType;
